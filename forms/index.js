a7febc2fd407376c68a15fb22164fe27ff908e88
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-02 10:57:10 +0300

    433

--- forms/index.js
+++ forms/index.js
@@ -1,3 +1,24 @@
+// // так можно получить данные формы - ВАРИАНТ 1:
+// const formField = document.querySelector(".login-form");
+// const formFields = [...new FormData(formElem)];
+// // formFields => [["email", "значение поля email"], ["password", "значение поля password"]]
+
+// const formData = formFields.reduce(function (acc, formField) {
+//   const prop = formField[0]; // здесь "name" инпута
+//   const value = formField[1]; // здесь "value" инпута
+//   // если использовать деструктуризацию, то можно предыдущие 2 строки записать короче
+//   // const [prop, value] = formField;
+//   return {
+//     // используем оператор расширения, чтобы в acc добвить свойства все предыдущих итераций
+//     ...acc,
+//     // используем вычислимое свойство объекта, чтобы добавить в аккумулятор новое свойство
+//     [prop]: value,
+//   };
+// }, {});
+
+// // более простой формат считывания формы - ВАРИАНТ 2:
+// // const formData = Object.fromEntries(new FormData(formElem));
+
 const emailInputElem = document.querySelector("#email");
 const passwordInputElem = document.querySelector("#password");
 
@@ -6,9 +27,9 @@ const passwordErrorElem = document.querySelector(".error-text_password");
 
 const formElem = document.querySelector(".login-form");
 
-const isRequaired = (value) => (value ? undefined : "Requaired");
+const isRequaired = (value) => (value ? undefined : "Required");
 const isEmail = (value) =>
-  value.includes("@") ? undefined : "Shoild be an email";
+  value.includes("@") ? undefined : "Should be an email";
 
 const validatorByField = {
   email: [isRequaired, isEmail],
