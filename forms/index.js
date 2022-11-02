b52e28fc52981aa240307d07b1002b51f426ceeb
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-02 10:36:12 +0300

    forms

---
+++ forms/index.js
@@ -0,0 +1,44 @@
+const emailInputElem = document.querySelector("#email");
+const passwordInputElem = document.querySelector("#password");
+
+const emailErrorElem = document.querySelector(".error-text_email");
+const passwordErrorElem = document.querySelector(".error-text_password");
+
+const formElem = document.querySelector(".login-form");
+
+const isRequaired = (value) => (value ? undefined : "Requaired");
+const isEmail = (value) =>
+  value.includes("@") ? undefined : "Shoild be an email";
+
+const validatorByField = {
+  email: [isRequaired, isEmail],
+  password: [isRequaired],
+};
+
+const validate = (fieldName, value) => {
+  const validators = validatorByField[fieldName];
+  return validators
+    .map((validator) => validator(value))
+    .filter((errorText) => errorText)
+    .join(", ");
+};
+
+const onEmailChange = (event) => {
+  const errorText = validate("email", event.target.value);
+  emailErrorElem.textContent = errorText;
+};
+
+const onPasswordChange = (event) => {
+  const errorText = validate("password", event.target.value);
+  passwordErrorElem.textContent = errorText;
+};
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const formData = Object.fromEntries(new FormData(formElem));
+  alert(JSON.stringify(formData));
+};
+
+emailInputElem.addEventListener("change", onEmailChange);
+passwordInputElem.addEventListener("change", onPasswordChange);
+formElem.addEventListener("submit", onFormSubmit);
