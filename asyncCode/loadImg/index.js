cad435685af2e84294bfa26d89060ea5d5a0577f
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-07 14:43:22 +0200

    2324

--- asyncCode/loadImg/index.js
+++ asyncCode/loadImg/index.js
@@ -28,8 +28,3 @@ const onImageLoaded = (error, imgElem) => {
 
   sizeElem.textContent = `${width} x ${height}`;
 };
-
-addImage(
-  "https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg",
-  onImageLoaded
-);
