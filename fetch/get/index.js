915f04f96268779cd2c4c54182e6f9ab7fd5cb34
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-08 20:49:53 +0200

    fetch

---
+++ fetch/get/index.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector(".name-form__btn");
+const avatarEl = document.querySelector(".user__avatar");
+const nameEl = document.querySelector(".user__name");
+const locationEl = document.querySelector(".user__location");
+const inputEl = document.querySelector(".name-form__input");
+
+const link = `https://api.github.com/users/`;
+let value = "";
+
+const getData = async (username) => {
+  return await fetch(`${link}${username}`).then((data) => data.json());
+};
+
+const onChangeHandler = (e) => {
+  value = e.target.value;
+};
+
+const renderNewData = (data) => {
+  const { avatar_url, name, location } = data;
+  nameEl.textContent = name;
+  avatarEl.src = avatar_url;
+  locationEl.textContent = location ? `from ${location}` : "";
+};
+
+const loadData = () => {
+  if (value === "") {
+    alert("Fill the field!");
+    return;
+  }
+  getData(value).then((data) => renderNewData(data));
+};
+
+inputEl.addEventListener("change", onChangeHandler);
+btn.addEventListener("click", loadData);
