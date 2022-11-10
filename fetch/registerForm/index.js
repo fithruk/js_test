635476ae79baa1ed6e2646c8902000cf08be90d2
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-10 15:04:19 +0200

    3434

---
+++ fetch/registerForm/index.js
@@ -0,0 +1,29 @@
+const formElem = document.querySelector(".login-form");
+const textFields = document.querySelectorAll(".login-form input");
+const buttonElem = document.querySelector(".submit-button");
+const url = "https://636b6df9ad62451f9fb14be5.mockapi.io/api/v1/users";
+
+const sentUserData = (userData) => {
+  return fetch("https://636b6df9ad62451f9fb14be5.mockapi.io/api/v1/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userData),
+  });
+};
+
+const checkValidtate = (e) => {
+  let boo = formElem.reportValidity();
+  boo ? (buttonElem.disabled = false) : (buttonElem.disabled = true);
+};
+
+const submitForm = (e) => {
+  e.preventDefault();
+  const formData = Object.fromEntries(new FormData(formElem));
+  sentUserData(formData)
+    .then((data) => data.json())
+    .then((data) => alert(JSON.stringify(data, null, 2)));
+  formElem.reset();
+};
+
+formElem.addEventListener("input", checkValidtate);
+buttonElem.addEventListener("click", submitForm);
