af89869238c3d18f67fd84f323e9f9e2beb5f4ee
Sergey Belov <sergeytrenerbelov@gmail.com>
2022-11-11 16:30:29 +0200

    3434

--- errorHandle/github_info/scripst/renderListItems.js
+++ errorHandle/github_info/scripst/renderListItems.js
@@ -1,6 +1,6 @@
 const repoListEl = document.querySelector(".repo-list");
 const renderListItems = (arrRepo) => {
-  return arrRepo.forEach(({ name }) => {
+  arrRepo.forEach(({ name }) => {
     const repoNameEl = document.createElement("li");
     repoNameEl.classList.add("repo-list__item");
     repoNameEl.textContent = name;
